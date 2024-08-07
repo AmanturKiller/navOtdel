632a33a0298030560b6e88bc29844d77110dcac1
KatanaEMT <abdylaev18@gmail.com>
2024-08-08 03:56:19 +0600

    added user model and modified settings

---
+++ user/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    bio = models.TextField(null=True, blank=True, verbose_name='биография')
+    USERNAME_FIELD = 'username'
