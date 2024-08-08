1fba1f5152148ecfd31af4691a393e4836c0d7e4
Эмир Абакиров <abakirovemir2008@gmail.com>
2024-08-08 11:42:23 +0600

    created department app, model

---
+++ department/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from user.models import User
+
+
+class Department(models.Model):
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    parent = models.ForeignKey(
+        to='self', 
+        verbose_name="Наследуется от",
+        null=True, blank=True, 
+        on_delete=models.CASCADE,
+    )
+    manager = models.ForeignKey(
+        verbose_name="Менеджер",
+        to=User,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = "Отдел",
+        verbose_name_plural = "Отделы",
+
+    def __str__(self):
+        return self.name
+    
