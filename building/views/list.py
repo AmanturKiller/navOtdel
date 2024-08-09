c25500aceb938b87d9e2dc38e23cb95a16cf0493
Эмир Абакиров <abakirovemir2008@gmail.com>
2024-08-09 21:05:17 +0600

    buildings list fixes

--- building/views/list.py
+++ building/views/list.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from building.models import *
+from building.filters import *
 
 
 def buildings_list(request):
     buildings_list = Building.objects.all()
-    context = {"buildings": buildings_list}
+    filter_object = BuildingFilter(
+        data = request.GET,
+        queryset = buildings_list
+    )
+    context = {"filter_object": filter_object}
     return render(request, 'building/list.html', context)
