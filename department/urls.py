d3957254adbf74227eea2a4ea7118cbd974b13cf
RZN1926 <kalygulovbeka7@gmail.com>
2024-08-09 12:27:30 +0600

    Added APIViewset for department

--- department/urls.py
+++ department/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from department.views import *
 
+user_router = routers.DefaultRouter()
+user_router.register("", DepartmentViewSet)
 
 urlpatterns = [
     path("list/", department_list, name="department-list"),
     path("info/<int:pk>/", department_detail, name="department-detail"),
+    path('', include(user_router.urls)),
 ]
--- department/views.py
+++ department/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from department.serializers import DepartmentSerializer
 from department.models import *
 from department.filters import *
 
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
 
 def department_list(request):
     department_list = Department.objects.all()
