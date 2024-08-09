493696b5841e215ecc8543e4e0426eff177a5a5a
danielb981 <bakirovd12@gmail.com>
2024-08-09 12:28:14 +0600

    add test status

--- department/tests.py
+++ department/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class DepartmentListTestCase(TestCase):
+    def test_department_list_status(self):
+        url = '/department/list/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200, "Страница со списком отделов не вернула статус 200")
