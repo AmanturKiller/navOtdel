71eb4f65e6ab29cd36f212431b63da11ef5a8503
danielb981 <bakirovd12@gmail.com>
2024-08-09 13:20:16 +0600

    add test admin

--- building/tests.py
+++ building/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+class AdminPageTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username='admin', password='password'
+        )
+
+    def test_admin_page_status(self):
+        self.client.login(username='admin', password='password')
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200, "Страница администратора не вернула статус 200")
