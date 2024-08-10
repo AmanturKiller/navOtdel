2d098ff00a81f280a60ded46afe2906d34af59ac
KatanaEMT <abdylaev18@gmail.com>
2024-08-11 00:51:08 +0600

    added redirect homepage

--- redinfo/urls.py
+++ redinfo/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from building.views.homepage import *
 
 urlpatterns = [
+    path('', homepage),
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
     path('department/', include("department.urls")),
