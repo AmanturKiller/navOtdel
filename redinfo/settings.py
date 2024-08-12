85f999216a32bd14e3f7724646658cd17f50e93c
Amankaium <kaium@list.ru>
2024-08-12 12:11:39 +0600

    add pagination

--- redinfo/settings.py
+++ redinfo/settings.py
@@ -137,3 +137,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
