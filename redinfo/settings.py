63ba615d7d8d875f5af3af58110c654b7a6e7463
Amankaium <kaium@list.ru>
2024-08-12 12:25:49 +0600

    preps for db migration

--- redinfo/settings.py
+++ redinfo/settings.py
@@ -87,6 +87,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "redinfo_db",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
